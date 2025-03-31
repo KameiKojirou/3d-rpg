ac00aeea4f4a9362c45dc429ad43f14a0572f904
Kojirou <54878605+KameiKojirou@users.noreply.github.com>
2025-03-30 21:41:34 -0600

    weapons and move in direction while attack

--- Player/player.gd
+++ Player/player.gd
@@ -4,14 +4,21 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+# sets the gravity from the project settings to be synced with RigidBody nodes
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-# this stores the x and y the player looks
+# Stores the x/y direction the player is trying to look in
 var _look := Vector2.ZERO
+# Stores the direction moves when attacking
+var _attack_direction := Vector3.ZERO
+
+
 @export var min_boundary: float = -60
 @export var max_boundary: float = 10
 @export var animation_decay: float = 20.0
 @export var mouse_sensitivity: float = 0.00075
+@export var attack_move_speed: float = 3.0
 
+@onready var rig: Node3D = $RigPivot/Rig
 @onready var horizontal_pivot: Node3D = $HorizontalPivot
 @onready var vertical_pivot: Node3D = $HorizontalPivot/VerticalPivot
 @onready var rig_pivot: Node3D = $RigPivot
@@ -34,6 +41,7 @@ func _physics_process(delta: float) -> void:
 	# var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	# var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	var direction := get_movement_direction()
+	rig.update_animation_tree(direction)
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -41,7 +49,7 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	handle_slashing_physics_frame(delta)
 	move_and_slide()
 
 
@@ -51,6 +59,9 @@ func _unhandled_input(event: InputEvent) -> void:
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
 			_look = - event.relative * mouse_sensitivity
+	if rig.is_idle():
+		if event.is_action_pressed("click"):
+			slash_attack()
 
 func get_movement_direction() -> Vector3:
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
@@ -76,3 +87,18 @@ func look_toward_direction(direction: Vector3, delta: float) -> void:
 		target_transform,
 		1.0 - exp(-animation_decay * delta)
 	)
+
+
+func slash_attack() -> void:
+	rig.travel("Slash")
+	_attack_direction = get_movement_direction()
+	if _attack_direction.is_zero_approx():
+		_attack_direction = rig_pivot.global_basis * Vector3(0, 0, 1)
+
+func handle_slashing_physics_frame(delta: float) -> void:
+	if not rig.is_slashing():
+		return
+
+	velocity.x = _attack_direction.x * attack_move_speed
+	velocity.z = _attack_direction.z * attack_move_speed
+	look_toward_direction(_attack_direction, delta)
