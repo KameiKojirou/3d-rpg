516ea9f4e07a436bc4055837667b3c074ab82c0a
Kojirou <54878605+KameiKojirou@users.noreply.github.com>
2025-04-06 14:28:53 -0600

    enemy_health and defeat animation wired up

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,11 +1,15 @@
 extends CharacterBody3D
+class_name Enemy
 
 @export var max_health: float = 20.0
 @onready var rig: Node3D = $Rig
 @onready var health_component: HealthComponent = $HealthComponent
 
 func _ready() -> void:
-    rig.set_active_mesh(
-        rig.villager_meshes.pick_random()
-    )
-    health_component.update_max_health(max_health)
+	rig.set_active_mesh(
+		rig.villager_meshes.pick_random()
+	)
+	health_component.update_max_health(max_health)
+
+func _on_health_component_defeat() -> void:
+	rig.travel("Defeat")
