5339be8b2f9eadc01b0d27c2af44dc048fb0414b
Kojirou <54878605+KameiKojirou@users.noreply.github.com>
2025-04-13 17:58:21 -0600

    inital dash work

---
+++ Player/dash.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+@export var player: Player
+@onready var timer: Timer = $Timer
+
+var direction := Vector3.ZERO
+
+
+func _unhandled_input(event: InputEvent) -> void:
+    if not timer.is_stopped():
+        return
+
+    if event.is_action_pressed("dash"):
+        direction = player.get_movement_direction()
+        if not direction.is_zero_approx():
+            print("we can dash")
+            timer.start(1.0)
